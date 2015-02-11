cbd1f24ba433fcba7b9a6a5c2098c330556d8f12
oohdeer <saracase04@gmail.com>
2015-02-11 13:08:03 -0500

    Fixed the brackets in javascript to square ones. Changed the font colours of dd and dt. Now just adding the berry name to each set

--- js/main.js
+++ js/main.js
@@ -1,46 +1,48 @@
 var blueBerries = {
-    latinName: 'vaccinium corymbosum',
-    growsOnShrub: true,
+    latin: 'vaccinium corymbosum',
+    growsOnShrub: 'Yes',
     energy: '240 kJ',
     carbohydrates: '14.49 g',
     protein: '0.74 g'
 };
 
 var grapes = {
-    latinName:'vitis vinifera',
-    growsOnShrub: false,
+    latin:'vitis vinifera',
+    growsOnShrub: 'No',
     energy: '288 kJ',
     carbohydrates: '18.1 g',
     protein: '0.72 g'
 };
 
 var redCurrant = {
-    latinName:'ribes rubrum',
-    growsOnShrub: 'true',
+    latin:'ribes rubrum',
+    growsOnShrub: 'Yes',
     energy: '234 kJ',
     carbohydrates: '13.8 g',
     protein: '1.4 g'
 };
 
-var fruitBerries = {blueBerries, grapes, redCurrant}
+var allBerries = [blueBerries, grapes, redCurrant];
 
-fruitBerries.forEach(function (item) {
+var writeBerries = function (allBerries) {
+  allBerries.forEach(function (item) {
   document.write('<dl>');
     
-  document.write('<dt>Latin Name</dt>');
-  document.write('<dd>' + item.latinName + '</dd>');
+  document.write('<dt>Latin:</dt>');
+  document.write('<dd>' + item.latin + '</dd>');
     
-  document.write('<dt>Grows On Shrubs</dt>');
+  document.write('<dt>Grows On Shrubs:</dt>');
   document.write('<dd>' + item.growsOnShrub + '</dd>');
     
-  document.write('<dt>Energy</dt>');
+  document.write('<dt>Energy:</dt>');
   document.write('<dd>' + item.energy + '</dd>');
     
-  document.write('<dt>Carbohydrates</dt>');
+  document.write('<dt>Carbohydrates:</dt>');
   document.write('<dd>' + item.carbohydrates + '</dd>');
     
-  document.write('<dt>Protein</dt>');
+  document.write('<dt>Protein:</dt>');
   document.write('<dd>' + item.protein + '</dd>');
     
   document.write('<dl>');
-});
+  });
+};
